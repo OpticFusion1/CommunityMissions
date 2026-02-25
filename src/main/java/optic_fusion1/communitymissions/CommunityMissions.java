06a95d12aa14e3c8a3975a2518df092881f5069c
OpticFusion1 <37254722+OpticFusion1@users.noreply.github.com>
2026-02-25 18:15:45 +0100

    Initial Commit

---
+++ src/main/java/optic_fusion1/communitymissions/CommunityMissions.java
@@ -0,0 +1,11 @@
+package optic_fusion1.communitymissions;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CommunityMissions extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+}
